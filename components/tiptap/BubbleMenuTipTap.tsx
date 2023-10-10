35f70b57a80c0f55477f5d75091a1f33910d0b3e
alexandre-dev28 <axel.business29@gmail.com>
2023-10-10 13:22:44 +0200

    adding language changes

--- components/tiptap/BubbleMenuTipTap.tsx
+++ components/tiptap/BubbleMenuTipTap.tsx
@@ -1,8 +1,23 @@
 import HandleImage from '@/components/tiptap/HandleImage';
 import HandleLinks from '@/components/tiptap/handleLinks';
+import HandleProgrammingLanguage from '@/components/tiptap/handleProgrammingLanguage';
 import { Toggle } from '@/components/ui/toggle';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 import { BubbleMenu, Editor } from '@tiptap/react';
-import { Bold, Edit, Italic, Link, Strikethrough } from 'lucide-react';
+import {
+  Bold,
+  CodeIcon,
+  Edit,
+  Italic,
+  LanguagesIcon,
+  Link,
+  Strikethrough,
+} from 'lucide-react';
 import React, { useState } from 'react';
 
 const isImageSelection = (editor: Editor | any) => {
@@ -15,6 +30,7 @@ const isImageSelection = (editor: Editor | any) => {
 
 const BubbleMenuTipTap = ({ editor }: { editor: Editor | null }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isLanguageOpen, setIsLanguageOpen] = useState(false);
   const [isModalImageOpen, setIsModalImageOpen] = useState(false);
 
   return (
@@ -26,12 +42,18 @@ const BubbleMenuTipTap = ({ editor }: { editor: Editor | null }) => {
           className={'bg-gray-200 dark:bg-gray-700 flex gap-2 p-2 rounded-md'}
         >
           <HandleLinks editor={editor} isOpen={isOpen} setIsOpen={setIsOpen} />
+          <HandleProgrammingLanguage
+            editor={editor}
+            isOpen={isLanguageOpen}
+            setIsOpen={setIsLanguageOpen}
+          />
           <HandleImage
             editor={editor}
             isOpen={isModalImageOpen}
             setIsOpen={setIsModalImageOpen}
             isEdit={true}
           />
+
           {!isImageSelection(editor) ? (
             <div className={'flex gap-2'}>
               <Toggle
@@ -62,6 +84,37 @@ const BubbleMenuTipTap = ({ editor }: { editor: Editor | null }) => {
               >
                 <Strikethrough className="h-4 w-4" />
               </Toggle>
+              <Toggle
+                onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+                variant={'outline'}
+                aria-label="Toggle Code block"
+                aria-pressed={`${
+                  editor.isActive('codeBlock') ? 'true' : 'false'
+                }`}
+                data-state={`${editor.isActive('codeBlock') ? 'on' : 'off'}`}
+              >
+                <CodeIcon className="h-4 w-4" />
+              </Toggle>
+              {editor.isActive('codeBlock') ? (
+                <Toggle
+                  onClick={() => setIsLanguageOpen(true)}
+                  variant={'outline'}
+                  aria-label="Change languages"
+                >
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <LanguagesIcon className="h-4 w-4" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Change Programming language</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Toggle>
+              ) : (
+                ''
+              )}
               <Toggle
                 onClick={() => {
                   if (editor.isActive('link')) {
