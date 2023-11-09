05a66024e1da2f36e1d69f53b387d7f99b122759
alexandre-dev28 <axel.business29@gmail.com>
2023-11-09 15:54:21 +0200

    rework the layout of the creation part

--- app/dashboard/posts/create/create-post-form.tsx
+++ app/dashboard/posts/create/create-post-form.tsx
@@ -37,7 +37,6 @@ import { Loader2, Save, XOctagon } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import Balancer from 'react-wrap-balancer';
 import * as z from 'zod';
 
 const formSchema = z.object({
@@ -105,192 +104,199 @@ const CreatePostForm = ({
   }
 
   return (
-    <div className={'flex gap-16'}>
-      <Card className={'h-fit'}>
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl">Create a new post</CardTitle>
-          <CardDescription>
-            Enter details below for the post you needs to create
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <div className={'grid grid-cols-1 gap-x-12 gap-y-4'}>
-                <FormField
-                  control={form.control}
-                  name="postTitle"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Title</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="ex: How to create an awesome blog"
-                          onKeyUp={(e: any) => {
-                            setTitle(e.target.value);
-                          }}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="categoryId"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Category</FormLabel>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select a category" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {categoryList.map((value) => (
-                            <SelectItem value={value.id} key={value.id}>
-                              {value.categoryName}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="postMainImage"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Post Image Url</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="ex: https://google.com"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <div className={'flex items-center gap-2'}>
-                  <div className={'flex-1'}>
-                    <FormField
-                      control={form.control}
-                      name="Tags"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Post Tags</FormLabel>
+    <>
+      <Form {...form}>
+        <div className={'flex gap-16'}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <Card className={'h-fit'}>
+              <CardHeader className="space-y-1">
+                <CardTitle className="text-2xl">Create a new post</CardTitle>
+                <CardDescription>
+                  Enter details below for the post you needs to create
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-4">
+                <div className={'grid grid-cols-1 gap-x-12 gap-y-4'}>
+                  <FormField
+                    control={form.control}
+                    name="categoryId"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Category</FormLabel>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
                           <FormControl>
-                            <Input placeholder="ex: NextJs" {...field} />
+                            <SelectTrigger>
+                              <SelectValue placeholder="Select a category" />
+                            </SelectTrigger>
                           </FormControl>
-                          <div>
-                            {postTags.length === 0 ? (
-                              <p className={'text-sm'}>
-                                Please Select a least one
-                              </p>
-                            ) : (
-                              postTags.map((value) => (
-                                <Badge key={value}>
-                                  {value}
-                                  <XOctagon
-                                    onClick={() => {
-                                      setPostTags([
-                                        ...postTags.filter(
-                                          (value1) => value1 != value
-                                        ),
-                                      ]);
-                                    }}
-                                    className={'ml-2 cursor-pointer'}
-                                  />
-                                </Badge>
-                              ))
-                            )}
-                          </div>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                  <Button
-                    type={'button'}
-                    onClick={() => {
-                      if (!form.getFieldState('Tags').invalid) {
-                        if (
-                          postTags.filter(
-                            (itemvalue) =>
-                              itemvalue.toLowerCase() ==
-                              form.getValues('Tags')?.toLowerCase()
-                          ).length === 0
-                        )
-                          setPostTags([
-                            ...postTags,
-                            `${form.getValues('Tags')?.toString()}`,
-                          ]);
-                      }
-                    }}
-                  >
-                    Add
-                  </Button>
-                </div>
-                <FormField
-                  control={form.control}
-                  name="postDescription"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Post Description</FormLabel>
-                      <FormControl>
-                        <Textarea
-                          onKeyUp={(event: any) => {
-                            setDescription(event.target.value);
-                          }}
-                          placeholder="Write a description"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
+                          <SelectContent>
+                            {categoryList.map((value) => (
+                              <SelectItem value={value.id} key={value.id}>
+                                {value.categoryName}
+                              </SelectItem>
+                            ))}
+                          </SelectContent>
+                        </Select>
 
-              <Button className="flex gap-4 self-end" disabled={isLoading}>
-                {!isLoading ? (
-                  <>
-                    <Save />
-                    Save
-                  </>
-                ) : (
-                  <>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                    Please wait
-                  </>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="postMainImage"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Post Image Url</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="ex: https://google.com"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <div className={'flex items-center gap-2'}>
+                    <div className={'flex-1'}>
+                      <FormField
+                        control={form.control}
+                        name="Tags"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Post Tags</FormLabel>
+                            <FormControl>
+                              <Input placeholder="ex: NextJs" {...field} />
+                            </FormControl>
+                            <div>
+                              {postTags.length === 0 ? (
+                                <p className={'text-sm'}>
+                                  Please Select a least one
+                                </p>
+                              ) : (
+                                postTags.map((value) => (
+                                  <Badge key={value}>
+                                    {value}
+                                    <XOctagon
+                                      onClick={() => {
+                                        setPostTags([
+                                          ...postTags.filter(
+                                            (value1) => value1 != value
+                                          ),
+                                        ]);
+                                      }}
+                                      className={'ml-2 cursor-pointer'}
+                                    />
+                                  </Badge>
+                                ))
+                              )}
+                            </div>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <Button
+                      type={'button'}
+                      onClick={() => {
+                        if (!form.getFieldState('Tags').invalid) {
+                          if (
+                            postTags.filter(
+                              (itemvalue) =>
+                                itemvalue.toLowerCase() ==
+                                form.getValues('Tags')?.toLowerCase()
+                            ).length === 0
+                          )
+                            setPostTags([
+                              ...postTags,
+                              `${form.getValues('Tags')?.toString()}`,
+                            ]);
+                        }
+                      }}
+                    >
+                      Add
+                    </Button>
+                  </div>
+                </div>
+
+                <Button className="flex gap-4 self-end" disabled={isLoading}>
+                  {!isLoading ? (
+                    <>
+                      <Save />
+                      Save
+                    </>
+                  ) : (
+                    <>
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                      Please wait
+                    </>
+                  )}
+                </Button>
+              </CardContent>
+            </Card>
+          </form>
+          <div className={'h-fit flex-1'}>
+            <div className="space-y-2">
+              <FormField
+                control={form.control}
+                name="postTitle"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Textarea
+                        rows={1}
+                        maxLength={120}
+                        cols={1}
+                        style={{ resize: 'none' }}
+                        className={cn(
+                          'scroll-m-20 min-h-[60px] text-3xl font-bold tracking-tight border-none outline-none focus-visible:outline-none focus-visible:border-none focus-visible:ring-0 p-0'
+                        )}
+                        placeholder="Article Title ..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
                 )}
-              </Button>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
-      <div className={'h-fit flex-1'}>
-        <div className="space-y-2">
-          <h1 className={cn('scroll-m-20 text-4xl font-bold tracking-tight')}>
-            {title}
-          </h1>
-          <p className="text-lg text-muted-foreground">
-            <Balancer>{description}</Balancer>
-          </p>
+              />
+              <FormField
+                control={form.control}
+                name="postDescription"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Textarea
+                        style={{ resize: 'none' }}
+                        rows={1}
+                        cols={1}
+                        maxLength={260}
+                        minLength={40}
+                        spellCheck={false}
+                        className={cn(
+                          'scroll-m-20 min-h-[60px] text-lg font-bold tracking-tight border-none outline-none focus-visible:outline-none focus-visible:border-none focus-visible:ring-0 p-0'
+                        )}
+                        placeholder="Write a description"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Separator className="my-4 md:my-6" />
+            <AwesomeEditor
+              editor={editor}
+              imageUploadFn={cloudinaryUploadImage}
+            />
+          </div>
         </div>
-        <Separator className="my-4 md:my-6" />
-        <AwesomeEditor editor={editor} imageUploadFn={cloudinaryUploadImage} />
-      </div>
-    </div>
+      </Form>
+    </>
   );
 };
 
