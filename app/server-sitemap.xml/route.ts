208d78155b6fa83e0085498fa8902f7cb6bd194d
alexandre-dev28 <axel.business29@gmail.com>
2023-11-30 19:52:43 +0200

    arrange sitemap

--- app/server-sitemap.xml/route.ts
+++ app/server-sitemap.xml/route.ts
@@ -1,6 +1,6 @@
 import { DbConnection, posts } from '@/db/src';
 import { eq } from 'drizzle-orm';
-import { getServerSideSitemap } from 'next-sitemap';
+import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
 
 export async function GET(request: Request) {
   // Method to source urls from cms
@@ -15,7 +15,13 @@ export async function GET(request: Request) {
         process.env.NEXT_PUBLIC_FRONT_URL || 'http://localhost:3000'
       }/post/${values.postSlug}`,
       lastmod: new Date().toISOString(),
-    };
+      news: {
+        date: values.publishedAt,
+        title: values.postTitle,
+        publicationLanguage: 'en',
+        publicationName: 'Axel Mwenze Blog',
+      },
+    } as ISitemapField;
   });
 
   return getServerSideSitemap([...fields]);
